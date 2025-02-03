68a7bbe66d707f476d70dd55232c93899165cefa
Frizio03 <fabrizio.tedeschi.scuola@gmail.com>
2025-02-03 16:25:32 +0100

    Update examples

--- examples/module-03/functions.js
+++ examples/module-03/functions.js
@@ -1,15 +1,15 @@
 //Calling a global function
 globalHello("John")
 
-//Defining a function with name f
+//Defining a function with name globalHello
 function globalHello(param){
     console.log("Hello " + param)
 }
 
-//Defining a function expression
+//Defining a function reference
 let myRef = globalHello;
 
-//Calling globalHello using the reference into myVar 
+//Calling globalHello using the reference into myRef 
 myRef("John");
 console.log(myRef)
 
@@ -19,6 +19,8 @@ console.log("---------------------------------")
 myExpression = function() {
     console.log("This is a function expression output")
 }
+
+//Calling function expression
 myExpression();
 console.log(myExpression)
 
@@ -28,4 +30,6 @@ console.log("---------------------------------")
 let myArrow = (a, b) => {
     console.log("Sum result:", a + b)
 }
+
+//Calling arrow function
 myArrow(10, 5)
